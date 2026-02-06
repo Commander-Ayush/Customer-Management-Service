78021fedcccff320824f499d66bec47b7a9f0ab7
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-02-06 17:32:24 +0530

    Initialized

--- src/main/java/com/bank/app/customermanagementservice/CustomerManagementServiceApplication.java
+++ src/main/java/com/bank/app/customermanagementservice/CustomerManagementServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerManagementServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerManagementServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagementServiceApplication.class, args);
+    }
 
 }
